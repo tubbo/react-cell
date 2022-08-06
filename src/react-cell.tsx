8f220086c5a22a9b98c8340c99775ab8d10b6a4b
Tom Scott <tscott@nerdstgamers.com>
2022-08-05 22:26:52 -0400

    add type defs for failure props and expose cell definition

--- src/react-cell.tsx
+++ src/react-cell.tsx
@@ -1,48 +1,60 @@
-import React from 'react';
-import { DocumentNode } from 'graphql';
-import { useQuery, ApolloError, QueryResult } from '@apollo/client';
+import React from 'react'
+import { DocumentNode } from 'graphql'
+import { useQuery, ApolloError, QueryResult } from '@apollo/client'
 
-interface CellDefinition<T, P> {
-  Empty?: React.FC<P & QueryRest<T>>;
-  Loading?: React.FC<P & QueryRest<T>>;
-  Failure?: React.FC<P & QueryRest<T> & { error: ApolloError }>;
-  Success: React.FC<P & T & QueryRest<T>>;
+type CellComponent<T, P> = (props: P & QueryRest<T>) => JSX.Element
+
+export interface FailureProps {
+  error: ApolloError
 }
 
-function isEmpty<T>(data: T) {
-  const field = (data as any)[Object.keys(data as any)[0]];
-  return (field == null) || (Array.isArray(field) && field.length == 0);
+export interface CellDefinition<T, P> {
+  Empty?: CellComponent<T, P>
+  Loading?: CellComponent<T, P>
+  Failure?: CellComponent<T, P & FailureProps>
+  Success: CellComponent<T, P>
+  isEmpty?: (data?: any) => boolean
 }
 
-type QueryRest<T> = Omit<QueryResult<T>, 'error'|'loading'|'data'>;
+type QueryRest<T> = Omit<QueryResult<T>, 'error' | 'loading' | 'data'>
 
-export default function createCell<T, P = {}>(query: DocumentNode, {
-  Success,
-  Empty = () => null,
-  Loading = () => null,
-  Failure = ({ error }) => {
-    console.error(error);
-    return null;
-  }
-}: CellDefinition<T, P>): React.FC<P> {
+export default function createCell<T, P = {}>(
+  query: DocumentNode,
+  {
+    Success,
+    Empty = () => <></>,
+    Loading = () => <></>,
+    Failure = ({ error }: FailureProps) => {
+      console.error(error)
+
+      return <></>
+    },
+    isEmpty = (data?: any) => {
+      if (!data) return false
+
+      const field = data[Object.keys(data)[0]]
+
+      return field == null || (Array.isArray(field) && field.length === 0)
+    }
+  }: CellDefinition<T, P>
+) {
   return (props: P) => {
-
     const { error, loading, data, ...rest } = useQuery<T>(query, {
       variables: props
-    });
+    })
 
     if (error) {
-      return <Failure error={error} {...props} {...rest} />;
+      return <Failure error={error} {...props} {...rest} />
     }
 
     if (loading) {
-      return <Loading {...props} {...rest} />;
+      return <Loading {...props} {...rest} />
     }
 
-    if (data == undefined || isEmpty(data)) {
-      return <Empty {...props} {...rest} />;
+    if (isEmpty(data)) {
+      return <Empty {...props} {...rest} />
     }
 
     return <Success {...data} {...props} {...rest} />
-  };
+  }
 }
